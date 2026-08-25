a545bd83cede93875ef365450a50ea8c8e614bde
jeet-24-9-7 <jeetgodhania2497@gmail.com>
2026-08-25 21:49:47 +0530

    Time: 31 ms (77.44%), Space: 48.1 MB (62.1%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeStars(String s) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '*') {
+                result.deleteCharAt(result.length() - 1);
+
+            } else if (ch != '*') {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
